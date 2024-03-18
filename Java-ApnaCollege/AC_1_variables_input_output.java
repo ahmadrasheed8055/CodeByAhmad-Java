868cf1f9553e972e5d92027193d120609b2da5f6
ahmadrasheed80 <ahmadrasheed8055@gmail.com>
2024-03-18 15:34:51 +0500

    Add sum and Input name

--- Java-ApnaCollege/AC_1_variables_input_output.java
+++ Java-ApnaCollege/AC_1_variables_input_output.java
@@ -6,5 +6,9 @@ public class AC_1_variables_input_output {
         Scanner sc = new Scanner(System.in);
         name = sc.nextLine();
         System.out.println(name);
+        int a = 10;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum of a and b : " + sum);
     }
 }
