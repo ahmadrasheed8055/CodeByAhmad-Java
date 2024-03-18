a5c97dcaf6b9dcbe1918f2b45e04020a3119ebc6
ahmadrasheed80 <ahmadrasheed8055@gmail.com>
2024-03-18 16:27:44 +0500

    Loops file added

---
+++ Java-ApnaCollege/AC_3_loops.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class AC_3_loops {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i < 12; i++) {
+//            System.out.println("Hello! world");
+//        }
+//
+        int  i = 1;
+        int num = 2;
+        while (i <= 10){
+            System.out.println(num + " * " + i + " = " + i * num);
+            i++;
+        }
+    }
+}
