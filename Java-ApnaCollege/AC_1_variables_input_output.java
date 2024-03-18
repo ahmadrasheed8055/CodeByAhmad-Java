ed3a3dad47523e16854ff8a57f15709a66170aa7
ahmadrasheed80 <ahmadrasheed8055@gmail.com>
2024-03-18 15:29:06 +0500

    Files Added

---
+++ Java-ApnaCollege/AC_1_variables_input_output.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class AC_1_variables_input_output {
+    public static void main(String[] args) {
+        String name ;
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        System.out.println(name);
+    }
+}
