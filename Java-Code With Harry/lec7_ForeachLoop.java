6b834a8acfdfe7a3d46e5850f0cc8ddc7f802e1b
Ahmad Rasheed <ahmadrasheed8055@gmail.com>
2024-03-19 23:07:22 +0500

    Foreach loop file added

---
+++ Java-Code With Harry/lec7_ForeachLoop.java
@@ -0,0 +1,9 @@
+public class lec7_ForeachLoop {
+    public static void main(String[] args) {
+        String [] names = {"Ahmad", "Ismat", "Umar", "Rizwan","Ayoub"};
+        System.out.println("Names : ");
+        for(String n : names){
+            System.out.print(n + " ");
+        }
+    }
+}
