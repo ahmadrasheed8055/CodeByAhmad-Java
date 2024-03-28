b2a94b9de6dbc5ad99e8f148075bc5ebe48127f0
Ahmad Rasheed <ahmadrasheed8055@gmail.com>
2024-03-28 16:30:05 +0500

    Packages creation

--- Java-VsCode/SecondClass.java
+++ Java-VsCode/SecondClass.java
@@ -1,7 +1,6 @@
+package second;
 
-package first;
-
-public class FirstClass {
+public  class SecondClass {
     public static void main(String[] args) {
        
        System.out.println("Second file of java");
