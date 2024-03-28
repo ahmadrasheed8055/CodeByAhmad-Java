d0c899852b69fb3af4a90f9fa8313058f679f421
Ahmad Rasheed <ahmadrasheed8055@gmail.com>
2024-03-28 21:45:13 +0500

    Package practice

---
+++ Java-VsCode/Calculation.java
@@ -0,0 +1,17 @@
+
+package calculator.add;
+
+
+public class Calculation {
+
+      public void add(int a , int b){
+            System.out.println("Addition of two num: " +( a + b));
+        }
+
+    public static void main(String[] args) {
+
+      
+       
+       System.out.println("Welcome to calculator!");
+    }
+}
---
+++ Java-VsCode/addition.java
@@ -0,0 +1,14 @@
+
+import calculator.add.Calculation;
+
+
+public class addition {
+   
+    public static void main(String[] args) {
+
+        Calculation a = new Calculation();
+        a.add(3,3);
+       
+       System.out.println("Welcome to calculator!");
+    }
+}
